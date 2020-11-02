ff4e3029e2c832579c64c466da19fb65cd6f96d4
User <rachida.haddouch.webdev@gmail.com>
2020-11-02 12:50:37 +0100

    my solution exo 5-11

--- 05-arrays/11-dedupe-array/script.js
+++ 05-arrays/11-dedupe-array/script.js
@@ -9,25 +9,35 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
-(function() {
-
+(function () {
     const fruits = [
-        "cerise",
-        "durian",
-        "pomme",
-        "poire",
-        "fraise",
-        "tomate",
-        "orange",
-        "mandarine",
-        "fraise",
-        "durian",
-        "pêche",
-        "cerise",
-        "raisin",
-        "cerise",
+      "cerise",
+      "durian",
+      "pomme",
+      "poire",
+      "fraise",
+      "tomate",
+      "orange",
+      "mandarine",
+      "fraise",
+      "durian",
+      "pêche",
+      "cerise",
+      "raisin",
+      "cerise",
     ];
-
-    // your code here
-
-})();
+  
+    // my solution 5-11
+    document.getElementById("run").addEventListener("click", function () {
+      let dedupe = [...new Set(fruits)];
+      console.log(dedupe);
+    });
+  })();
+  
+  /*  other way 
+  document.getElementById("run").addEventListener("click", function () {
+      let dedupe = fruits.filter((item, index) => fruits.indexOf(item) === index);
+      console.log(dedupe);
+    });
+  
+   */
