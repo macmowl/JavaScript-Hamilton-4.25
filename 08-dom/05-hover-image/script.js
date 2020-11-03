55fc7dc7a18e0730fb63259504e8f32f49b49cf5
Magali Garot <magali.garot@gmail.com>
2020-11-03 13:28:41 +0100

    Mysolution8.5

--- 08-dom/05-hover-image/script.js
+++ 08-dom/05-hover-image/script.js
@@ -11,6 +11,20 @@
 
 (function() {
 
-    // your code here
+      //your code here
+      let nocoeur = document.getElementsByTagName("IMG")[0].getAttribute("src"); 
+      //prend l'élement IMG 0 avec l'attribut src 
+      let coeur = document.getElementsByTagName("IMG")[0].getAttribute("data-hover");  
+      //prend l'élement IMG 0 avec le nom data-hover 
+  
+      document.getElementsByTagName("img")[0].addEventListener("mouseover", () => { 
+          //ajoute un évenement mouseover, quand on passe la souris l'image date-hover (coeur) apparait
+          document.getElementsByTagName("img")[0].src = coeur;
+      });
+      document.getElementsByTagName("img")[0].addEventListener("mouseout", () => {
+           //ajoute un évenement mouseout, quand la souris quitte l'image l'image nocoeur revient
+           //si cette action n'est pas ajoutée l'image data-hover/coeur reste 
+          document.getElementsByTagName("img")[0].src = nocoeur;
+      });
 
 })();
