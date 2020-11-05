8f55de72b0b95f3b0d4c45c767cf31fe2ae77bb8
iCarolinei <iCarolinei@github.com>
2020-11-05 16:26:18 +0100

    My solution 8.11

--- 08-dom/11-change-event-input-one/script.js
+++ 08-dom/11-change-event-input-one/script.js
@@ -10,7 +10,16 @@
 // You will have time to focus on it later.
 
 (function() {
+    document.getElementById("pass-one").setAttribute("maxLength",10) ; /*max 10 sur l'élément elements*/
+    document.getElementById("pass-one").addEventListener( "input", () => {
+        /*Create variable with id HTML et les invoquer*/
+    let inputElement = document.getElementById("pass-one");/*element input*/
+    let counterElement = document.getElementById("counter"); /*0/10*/
+    /*action*/
+     counterElement.innerText = `${inputElement.value.length}/10`;
 
-    // your code here
+    });
 
 })();
+
+
