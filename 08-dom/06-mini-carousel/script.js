8690987d0ba1c85eef4310c8ec0aaea4b8b34508
Gaëtan Massart <gtn.massart@gmail.com>
2020-11-09 09:59:45 +0100

    correction exo8.6 gaetan

--- 08-dom/06-mini-carousel/script.js
+++ 08-dom/06-mini-carousel/script.js
@@ -11,7 +11,7 @@
 
 (function() {
 
-    var gallery= [
+    let gallery = [
         "../../_shared/img/bell.svg",
         "../../_shared/img/clock.svg",
         "../../_shared/img/compass.svg",
@@ -21,4 +21,24 @@
 
     // your code here
 
+    let index = 0;
+    let parent = document.querySelector(".material");
+    let image = parent.getElementsByTagName("img")[0];
+
+
+    document.getElementById("next").addEventListener("click", ()=>{
+
+        index++;
+        image.src = gallery[index];
+
+        if (index > gallery.length-2) {
+            index = -1;
+        }  else {
+        }   
+        
+        console.log(gallery);
+        console.log(typeof(gallery));
+        console.log(image.src);
+
+    })
 })();
