1da9e4e6a6b5ac2adb8532067b75719f92c8d732
leny <info@flatland.be>
2020-11-09 14:55:34 +0100

    Fix 4.2

--- 04-dates/02-title-by-hour-two/script.js
+++ 04-dates/02-title-by-hour-two/script.js
@@ -10,9 +10,22 @@
 // You will have time to focus on it later.
 
 (function() {
-
     // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
 
     // your code here
 
+    const now = new Date();
+    const hours = now.getHours();
+    const minutes = now.getMinutes();
+    const time = hours * 100 + minutes; // 1h30 -> 130 ; 22h45 -> 2245
+
+    let greet;
+
+    if (time < 1730) {
+        greet = "Hello";
+    } else {
+        greet = "Good evening";
+    }
+
+    document.getElementById("element-id").innerHTML = greet;
 })();
