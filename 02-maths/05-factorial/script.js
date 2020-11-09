8f2c307eebfa32301f10ffd1212aae61a081ce4c
leny <info@flatland.be>
2020-11-09 14:21:31 +0100

    Fix 2.5

--- 02-maths/05-factorial/script.js
+++ 02-maths/05-factorial/script.js
@@ -9,26 +9,23 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+function factorialize(num) {
+    //   return 1;
+    // for -boucle
+    for (let i = num - 1; i >= 1; i--) {
+        num *= i;
+    }
+    alert(num);
+}
+
 (function() {
-    
     // to get the value of an input: document.getElementById("element-id").value
 
-document.getElementById("run").addEventListener("click", function() {
-
+    document.getElementById("run").addEventListener("click", function() {
         let num = document.getElementById("number").value;
         /*alert(num);   Just for test getlement*/
 
         factorialize(num);
- 
     });
-
 })();
 
-function factorialize(num) {
-    //   return 1;
-    // for -boucle 
-    for (var i = num - 1; i >= 1; i--) {
-      num *= i;
-    }
-    alert(num);
-  }
