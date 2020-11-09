62dac215373d0725686a96d99e3ddacace81ceea
SandrineLe <le.sandrinemt@gmail.com>
2020-11-09 09:44:39 +0100

    added correction exo_02.04

--- 02-maths/04-sort-numbers/script.js
+++ 02-maths/04-sort-numbers/script.js
@@ -16,6 +16,9 @@
     document.getElementById("run").addEventListener("click", function() {
 
         // your code here
+        let numbers = [2, 4, 14, 10, 90, 23, 16]
+let sorted = numbers.sort((a,b)=>a-b)
+alert(sorted) 
 
     });
 
