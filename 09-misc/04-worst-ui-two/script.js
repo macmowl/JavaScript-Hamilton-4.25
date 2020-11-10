5cd68c8cccc2f61e254e9d6674bea79adaa837e4
leny <info@flatland.be>
2020-11-10 11:44:53 +0100

    Fix 9.4

--- 09-misc/04-worst-ui-two/script.js
+++ 09-misc/04-worst-ui-two/script.js
@@ -9,23 +9,23 @@
 // NOTE: don't focus on the existing code structure for now.
 // You will have time to focus on it later.
 
+function updateDisplay(parts) {
+    document.getElementById("target").innerText = `+${parts[0].innerText}${
+        parts[1].innerText
+    }${parts[2].innerText}${parts[3].innerText}`;
+}
+
 (function() {
+    const parts = Array.from(document.querySelectorAll("button"));
 
-    const parts = [
-        document.getElementById("part-one"),
-        document.getElementById("part-two"),
-        document.getElementById("part-three"),
-        document.getElementById("part-four")
-    ];
-
-    parts.forEach( (x,i) => x.addEventListener("click", () => {
-        x.innerText===x.getAttribute("data-max") ? x.innerText = x.getAttribute("data-min") : ++x.innerText ;
-        x.innerText = i ? ("0"+x.innerText).slice(-2) : ("0"+x.innerText).slice(-3);
-        updateDisplay();
-    }));
-
-    function updateDisplay(){
-        document.getElementById("target").innerText = `+${parts[0].innerText}${parts[1].innerText}${parts[2].innerText}${parts[3].innerText}`;
-    }
-
+    parts.forEach((elt) =>
+        elt.addEventListener("click", () => {
+            elt.innerText =
+                elt.innerText === elt.getAttribute("data-max")
+                    ? elt.getAttribute("data-min")
+                    : +elt.innerText + 1;
+            elt.innerText = elt.innerText.padStart(2, "0");
+            updateDisplay(parts);
+        }),
+    );
 })();
