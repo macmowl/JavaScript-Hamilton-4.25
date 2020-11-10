0e0f34cacca6da2255ae330bc6bed734ac98d560
leny <info@flatland.be>
2020-11-10 10:47:55 +0100

    Fix 8.11

--- 08-dom/11-change-event-input-one/script.js
+++ 08-dom/11-change-event-input-one/script.js
@@ -1,14 +1,10 @@
 (function() {
-    document.getElementById("pass-one").setAttribute("maxLength",10) ; /*max 10 sur l'élément elements*/
-    document.getElementById("pass-one").addEventListener( "input", () => {
-        /*Create variable with id HTML et les invoquer*/
-    let inputElement = document.getElementById("pass-one");/*element input*/
-    let counterElement = document.getElementById("counter"); /*0/10*/
-    /*action*/
-     counterElement.innerText = `${inputElement.value.length}/10`;
+    const input = document.getElementById("pass-one");
+    const counter = document.getElementById("counter");
 
+    input.setAttribute("maxLength", 10); /*max 10 sur l'élément elements*/
+
+    document.getElementById("pass-one").addEventListener("input", () => {
+        counter.innerText = `${input.value.length}/10`;
     });
-
 })();
-
-
