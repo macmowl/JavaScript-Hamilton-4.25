f379a1efc2a8fcd89b8c18dc110863d399cd94f9
leny <info@flatland.be>
2020-11-10 10:30:50 +0100

    Fix 8.7

--- 08-dom/07-generate-table-one/script.js
+++ 08-dom/07-generate-table-one/script.js
@@ -11,17 +11,16 @@
 
 (function() {
 
-        let tableau = document.createElement("table");  //crée une table.
-        let row, data;                                  //crée les variables "row" et "data".
+        const table = document.createElement("table");  //crée une table.
 
         for (let i = 0; i < 10; i++) {                  //crée une boucle for pour créer 10 lignes.
-            row = document.createElement("tr");         //crée le html <tr>.
-            data = document.createElement("td");        //crée le html <td>.
-            data.innerText = "Bastien";                 //crée la chaine de caractère "Bastien".
-            row.appendChild(data);                      //pousse l'enfant "data" dans le parent "row".
-            tableau.appendChild(row);                   //pousse l'enfant "row" dans le parent "tableau".
+            const row = document.createElement("tr");         //crée le html <tr>.
+            const cell = document.createElement("td");        //crée le html <td>.
+            cell.innerText = i+1;                 // on affiche l'index dans la cellule
+            row.appendChild(cell);                      //pousse l'enfant "data" dans le parent "row".
+            table.appendChild(row);                   //pousse l'enfant "row" dans le parent "tableau".
         }
 
-        document.getElementById("target").appendChild(tableau); //pousse l'enfant "tableau" dans le parent "target".
+        document.getElementById("target").appendChild(table); //pousse l'enfant "tableau" dans le parent "target".
 
 })();
